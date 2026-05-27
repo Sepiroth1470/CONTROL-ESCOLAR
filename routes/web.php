ccdc54ca071c425c8da18ab3d043b8ca32f06936
NataliaJaime <nataliajaime210104@gmail.com>
2026-05-27 15:28:04 -0600

    se agrega codigo a web

--- routes/web.php
+++ routes/web.php
@@ -1,64 +1,26 @@
 <?php
-session_start();
-include("conexion.php");
 
-if(isset($_POST['login'])){
+/*
+|--------------------------------------------------------------------------
+| routes/web.php
+|--------------------------------------------------------------------------
+| Aquí se definen las rutas web de la aplicación
+|
+*/
 
-    $usuario = $_POST['usuario'];
-    $password = $_POST['password'];
+use Illuminate\Support\Facades\Route;
 
-    $query = "SELECT * FROM usuarios 
-              WHERE usuario='$usuario' 
-              AND password='$password'";
+// Ruta principal
+Route::get('/', function () {
+    return view('welcome');
+});
 
-    $resultado = mysqli_query($conn,$query);
+// Ruta llamada "route"
+Route::get('/route', function () {
+    return "Esta es la página route";
+});
 
-    if(mysqli_num_rows($resultado) > 0){
-
-        $datos = mysqli_fetch_assoc($resultado);
-
-        $_SESSION['usuario'] = $datos['nombre'];
-        $_SESSION['rol'] = $datos['rol'];
-
-        header("Location: index.php");
-
-    }else{
-        echo "Datos incorrectos";
-    }
-}
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-
-<body class="bg-dark">
-
-<div class="container mt-5">
-
-    <div class="card p-4">
-
-        <h2 class="text-center">Sistema Escolar</h2>
-
-        <form method="POST">
-
-            <input type="text" name="usuario" class="form-control mb-3" placeholder="Usuario">
-
-            <input type="password" name="password" class="form-control mb-3" placeholder="Contraseña">
-
-            <button type="submit" name="login" class="btn btn-primary w-100">
-                Ingresar
-            </button>
-
-        </form>
-
-    </div>
-
-</div>
-
-</body>
-</html>
+// Ruta llamada "resources"
+Route::get('/resources', function () {
+    return "Esta es la página resources";
+});
