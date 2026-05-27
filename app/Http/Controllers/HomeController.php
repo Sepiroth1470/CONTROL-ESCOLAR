eaf3961092aa56f69bcdd8f61fe28e82848e711d
SebastianGonzalez <chinogandalla2503@gmail.com>
2026-05-27 16:38:51 -0600

    Se agrega la modificación del HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,1 +1,22 @@
+<?php
 
+class HomeController
+{
+    public function index()
+    {
+        echo "<h1>Sistema de Control Escolar</h1>";
+        echo "<h2>Menú principal</h2>";
+
+        echo "<ul>";
+        echo "<li><a href='#'>Alumnos</a></li>";
+        echo "<li><a href='#'>Profesores</a></li>";
+        echo "<li><a href='#'>Administrativos Nivel A</a></li>";
+        echo "<li><a href='#'>Administrativos Nivel B</a></li>";
+        echo "<li><a href='#'>Director de Carrera</a></li>";
+        echo "<li><a href='#'>Secretario Académico</a></li>";
+        echo "</ul>";
+    }
+}
+
+$home = new HomeController();
+$home->index();
