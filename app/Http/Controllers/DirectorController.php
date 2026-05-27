17078bcd2593dd7a72b901fc6c9b1bffa746babe
kiyoshi <kiyoshi@>
2026-05-27 16:45:42 -0600

    se hizo cambios en controlador director

--- app/Http/Controllers/DirectorController.php
+++ app/Http/Controllers/DirectorController.php
@@ -1,1 +1,27 @@
 
+<?php
+
+class DirectorController {
+
+    // Mostrar información
+    public function mostrarDirector() {
+        echo "Mostrando director";
+    }
+
+    // Agregar director
+    public function agregarDirector() {
+        echo "Director agregado";
+    }
+
+    // Actualizar director
+    public function actualizarDirector() {
+        echo "Director actualizado";
+    }
+
+    // Eliminar director
+    public function eliminarDirector() {
+        echo "Director eliminado";
+    }
+}
+
+?>
